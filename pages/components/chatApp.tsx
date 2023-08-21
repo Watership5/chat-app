b7406f59d6ae7806cdccca5230f0e30e647b9cfe
crow <AverageGoku@outlook.com>
2023-08-21 16:07:06 -0500

    update
    update

--- pages/components/chatApp.tsx
+++ pages/components/chatApp.tsx
@@ -1,5 +1,5 @@
 import { signOut } from "firebase/auth"
-import { collection, getDocs, addDoc } from "firebase/firestore"
+import { collection, addDoc, deleteDoc, doc, onSnapshot } from "firebase/firestore"
 import { auth,db } from "../config/firebase"
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
@@ -7,6 +7,8 @@ import { useState, useEffect, useRef } from "react"
 const ChatApp = () => {
   const [messages, setMessages] = useState([])
   const [input, setInput] = useState("")
+  const [index, setIndex] = useState(null)
+  const [counter, setCounter] = useState(1)
   const bar = useRef()
   console.log(input)
   const SignOut = async () => {
@@ -17,25 +19,43 @@ const ChatApp = () => {
     }
   }
   const dummy = useRef()
-  const { uid } = auth.currentUser
-  const messageCollectionRef = collection(db, "messages")
-  const getMessages = async () => {
-    try{
-      const data = await getDocs(messageCollectionRef)
-      const filteredData = data.docs
-      .map((doc) => ({
-        ...doc.data(),
-        id: doc.id
-      }))
-      .sort((a, b) => a.createdAt - b.createdAt);
-      console.log(filteredData)
-      setMessages(filteredData)
-      dummy.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'end' });
-    } catch(err){
-      console.log(err)
-    }
-  };  
+  const { uid, photoURL } = auth.currentUser
+  const messageCollectionRef = collection(db, "msgs")
+  // const getMessages = async () => {
+  //   try{
+  //     const data = await getDocs(messageCollectionRef)
+  //     const filteredData = data.docs
+  //     .map((doc) => ({
+  //       ...doc.data(),
+  //       id: doc.id
+  //     }))
+  //     .sort((a, b) => a.createdAt - b.createdAt);
+  //     console.log(filteredData)
+  //     setMessages(filteredData)
+  //   } catch(err){
+  //     console.log(err)
+  //   }
+  // }; 
+  const getMessages = () => {
+    const unsubscribe = onSnapshot(messageCollectionRef,
+      (snapshot) => {
+        const filteredData = snapshot.docs
+          .map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+          }))
+          .sort((a, b) => a.createdAt - b.createdAt);
+        setMessages(filteredData);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+    return unsubscribe;
+  }; 
   const sendMessage = async (e) => {
+    setCounter(counter+1)
+    dummy.current.scrollIntoView({ behavior: 'smooth', block: 'end', offset: {top:200, left:0} });
     e.preventDefault();
     if(input.trim() === "" || input === null){
       console.log("blank")
@@ -46,6 +66,8 @@ const ChatApp = () => {
           message: input,
           createdAt: firebase.firestore.FieldValue.serverTimestamp(),
           uid,
+          photoURL,
+          index: counter
         });
       } catch (err) {
         console.error(err);
@@ -55,25 +77,43 @@ const ChatApp = () => {
       setInput("")
     }
   };
+  const deleteMsg = async (id, index) => {
+    setIndex(index)
+    try{
+      const msg = doc(db, "msgs", id)
+      await deleteDoc(msg)
+      getMessages()
+    }
+    catch(err){
+      console.error(err)
+    }
+  }
   useEffect(() => {
     getMessages();
   }, [])
   return (
     <main className="h-[100vh]">
-      <nav className="flex items-center justify-between py-[1.5rem] px-[3rem] gap-[1rem] w-[100%] text-white text-[35px]">
-        <p className="text-[30px]">ChatApp ✅</p>
+      <nav className="flex items-center justify-between py-[1.5rem] px-[3rem] pt-[1rem] gap-[1rem] w-[100%] text-white text-[35px]">
+        <button className="text-[30px]" onClick={SignOut}>ChatApp ✅</button>
         <button onClick={SignOut} className="p-[1rem]">
           ✌️
         </button>
       </nav>
-      <div className="h-[72vh] overflow-auto mb-[13rem] flex flex-col gap-[0.75rem] relative">
+      <div className="h-[65vh] overflow-auto mb-[13rem] flex flex-col gap-[0.5rem] relative">
         {messages.map((message) => (
           <div key={message.id} className="flex flex-col relative">
-            <p className={message.uid == auth.currentUser?.uid ? "cursor-pointer mr-[3rem] px-[1rem] py-[15px] bg-[#4c956c] text-white w-fit self-end" : "mb-[0.5rem] overflow-y-auto cursor-pointer ml-[3rem] px-[1rem] py-[15px] bg-[#e63946] text-white w-fit"}>{message.message}</p>
-            <span ref={dummy} className="">
-            </span>
+            <div className={message.uid == auth.currentUser?.uid ? "flex flex-row-reverse items-center" : "flex flex-row items-center"}>
+              <p onDoubleClick={() => deleteMsg(message.id, message.index)} className={message.uid == auth.currentUser?.uid ? "cursor-pointer mr-[3rem] px-[1rem] py-[15px] bg-[#4c956c] text-white w-fit text-[13px] self-end" : "mb-[0.5rem] text-[13px] overflow-y-auto cursor-pointer ml-[3rem] self-start px-[1rem] py-[15px] bg-[#e63946] text-white w-fit"}>{message.message}</p>
+              <img src={message.photoURL || 'https://wallpapers-clan.com/wp-content/uploads/2022/12/anonymous-pfp-36.jpg'} alt="" width={50} className={message.uid == auth.currentUser?.uid ? "h-fit self-end border-[#4c956c] border-[.134rem] mr-[10px] float-right" : "h-fit self-start border-[#e63946] border-[.134rem] ml-[10px] float-right"}/>
+            </div>
           </div>
         ))}
+        <span className="text-transparent text-[20px]">
+          hi
+        </span>
+        <span ref={dummy} className="text-transparent text-[20px]">
+          asd
+        </span>
       </div>
       <form className="flex items-end justify-center w-[100% bottom-[10rem] relative border-white px-[3rem]" onSubmit={sendMessage}>
         <input id="input" ref={bar} type="text" placeholder="Hi Mom!" onChange={(e) => setInput(e.target.value)} className="w-[100%] h-[4rem] pl-[1rem] bg-black-two border-[0px] cursor-pointer outline-none text-white placeholder:text-white placeholder:text-[20px] text-[20px]"/>
